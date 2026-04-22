8b080ebd7cb7eef2cb3bd7445fe6e17ab11e6d77
Koos85 <koosjoosten@gmail.com>
2026-04-22 13:31:08 +0200

    class based checks

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from libprobe.probe import Probe
-from lib.check.organization import check_organization
-from lib.check.wireless import check_wireless
+from lib.check.organization import CheckOrganization
+from lib.check.wireless import CheckWireless
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
-    checks = {
-        'organization': check_organization,
-        'wireless': check_wireless,
-    }
+    checks = (
+        CheckOrganization,
+        CheckWireless,
+    )
 
     probe = Probe("merakiorg", version, checks)
 
